ae798d94ba5bfa1b5c268fd7a9b633898ab6917f
Allan Blair <amaxwellblair@gmail.com>
2025-02-21 12:12:51 -0800

    Update date filter to handle ranges smaller than a month

--- src/bframelib/client_sql/dates.sql
+++ src/bframelib/client_sql/dates.sql
@@ -1,6 +1,6 @@
 SELECT *
 FROM src.dates
 {% if _BF_RATING_RANGE_START != '' and _BF_RATING_RANGE_END != '' %}
-WHERE _BF_RATING_RANGE_START <= month_start 
+WHERE date_trunc('month', CAST(_BF_RATING_RANGE_START as TIMESTAMP)) <= month_start 
     AND month_start < _BF_RATING_RANGE_END
 {% endif %}
--- tests/configuration/test_dates.py
+++ tests/configuration/test_dates.py
@@ -19,3 +19,13 @@ class TestDateRange:
         """).fetchall()
 
         assert len(dates) == 6
+
+    def test_date_range_smaller_than_a_month(self, client: Client):
+        client.set_config({'rating_range': ['2023-01-02', '2023-02-01']})
+        dates = client.execute("""
+            select *
+            from bframe.dates
+            where month_start between '2023-01-01' and '2023-12-01';
+        """).fetchall()
+
+        assert len(dates) == 1

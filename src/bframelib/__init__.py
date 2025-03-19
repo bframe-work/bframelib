76614f74c3265a254f0b46700b231ae8218f5d42
Allan Blair <amaxwellblair@gmail.com>
2025-03-19 16:25:54 -0700

    Update version

--- src/bframelib/__init__.py
+++ src/bframelib/__init__.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 
 PATH = os.path.dirname(__file__)
-__version__ = "0.1.18"
+__version__ = "0.1.19"
 
 # PATH must be exported first since subsequent modules reference it
 from .client import Client, Source, DEFAULT_SOURCES
